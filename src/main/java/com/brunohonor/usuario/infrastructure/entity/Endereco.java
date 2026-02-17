ff1cd652cc38adc5780b8a99ca53533096316d59
Bruno <brunohonor@hotmail.com>
2026-02-17 09:55:39 -0300

    adicionada classes de security

---
+++ src/main/java/com/brunohonor/usuario/infrastructure/entity/Endereco.java
@@ -0,0 +1,42 @@
+package com.brunohonor.usuario.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "endereco")
+public class Endereco {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "rua")
+    private String rua;
+    @Column(name = "bairro")
+    private String bairro;
+    @Column(name = "cep" , length = 9)
+    private String cep;
+    @Column(name = "numero")
+    private String numero;
+    @Column(name = "complemento")
+    private String complemento;
+    @Column(name = "cidade" , length = 20)
+    private String cidade;
+    @Column(name = "estado", length = 2)
+    private String estado;
+
+//    @ManyToOne
+//    @JoinColumn(name = "idusuario")
+//    private Usuario usuario;
+
+
+
+
+}
---
+++ src/main/java/com/brunohonor/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.brunohonor.usuario.infrastructure.repository;
+
+import com.bruno.aprendendo.spring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
